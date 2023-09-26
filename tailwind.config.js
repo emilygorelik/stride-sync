9b3b9e4c07d3076e36b8aa98087fff075eb8a573
Emily Gorelik <egorelik@jahnelgroup.com>
2023-09-26 14:10:15 -0400

    added spotify green as variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        spotify: '#1db954',
+      },
+    },
   },
   plugins: [require('daisyui')],
 };
