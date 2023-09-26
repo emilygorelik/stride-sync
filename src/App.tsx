35018c039c41731b4b72ee0dbf50ca55b08090c6
Emily Gorelik <egorelik@jahnelgroup.com>
2023-09-26 13:06:35 -0400

    remove all content

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,5 @@
-import { useState } from "react";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1 className="font-bold text-2xl">Vite + React</h1>
-      <div className="card">
-        <button
-          className="bg-green-500"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          counter: {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
