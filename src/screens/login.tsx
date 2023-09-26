c7f160829eee476211a9662a7af77868a5dbe73d
Emily Gorelik <egorelik@jahnelgroup.com>
2023-09-26 14:14:35 -0400

    styling

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,12 +1,14 @@
 function Login() {
   return (
-    <div className="flex bg-purple-950 w-[100vw] h-[100vh] justify-center items-center">
-      <div className="flex flex-col bg-white w-fit h-min p-6 rounded-2xl text-center">
+    <div className="flex bg-purple-700 w-[100vw] h-[100vh] justify-center items-center">
+      <div className="flex flex-col bg-white w-fit h-min p-6 rounded-2xl text-center drop-shadow-xl">
         <h1 className="h1 text-black">Spotify StrideSync</h1>
         <p className="italic p-4">
           The perfect playlist <br /> for your next run
         </p>
-        <button className="btn bg-spotify">login with spotify</button>
+        <button className="btn bg-spotify text-white border-none rounded-full text-lg">
+          login with spotify
+        </button>
       </div>
     </div>
   );
