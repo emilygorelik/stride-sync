d8865723457f365e4863264812dc0ebe6a3408ee
Emily Gorelik <egorelik@jahnelgroup.com>
2023-09-28 16:20:55 -0400

    clean up

--- src/screens/callback.tsx
+++ src/screens/callback.tsx
@@ -7,50 +7,34 @@ function Callback() {
   const [searchParams, setSearchParams] = useSearchParams();
   const { loginWithSpotify, accessToken } = useContext(UserTokenContext);
   const code = searchParams.get('code');
-  const [user, setUser] = useState();
+  const [user, setUser] = useState([]);
   const { isLoading } = useQuery(
     ['taskLists'],
     () => fetchProfile(accessToken),
     {
       onSuccess: (user) => {
         setUser(user);
-        console.log('-------------');
-        console.log(user);
       },
     },
   );
 
   if (isLoading) {
-    return (
-      <p>
-        please wait
-        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-      </p>
-    );
+    return <></>;
   }
   if (code && !accessToken) {
     loginWithSpotify(code);
     return (
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col h-screen justify-center items-center">
         Home Page WIP
-        <br></br>
-        {searchParams.get('code')}
-        <br></br>
-        weeeeeeeee
-        <br></br>
-        {accessToken}
       </div>
     );
   }
 
   return (
-    <div>
-      {accessToken}
-      <br />
-      {user?.display_name}
+    <div className="flex flex-col h-screen justify-center items-center">
+      Hello there {user.display_name}
     </div>
   );
-  //return <Navigate to="/"></Navigate>;
 }
 
 export default Callback;
