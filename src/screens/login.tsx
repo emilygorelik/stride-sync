541ae0973ededa7889257d09b0a9e42275822209
Emily Gorelik <egorelik@jahnelgroup.com>
2023-09-28 13:01:48 -0400

    simplify styles

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -5,7 +5,7 @@ function Login() {
   return (
     <div className="flex w-[100vw] h-[100vh] justify-center items-center">
       <div className="flex flex-col bg-white w-fit h-min p-6 rounded-2xl text-center items-center drop-shadow-xl">
-        <h1 className="h1 text-black">Spotify StrideSync</h1>
+        <h1 className="text-black">Spotify StrideSync</h1>
         <p className="italic p-4 w-3/4 text-gray-400">
           The perfect playlist for your next run
         </p>
