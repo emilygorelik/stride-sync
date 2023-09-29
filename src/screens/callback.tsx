e9e5c74cfd1dccbb0fb45992907c8c6a95361a1c
Emily Gorelik <egorelik@jahnelgroup.com>
2023-09-29 15:08:10 -0400

    tried some stuff

--- src/screens/callback.tsx
+++ src/screens/callback.tsx
@@ -3,11 +3,38 @@ import { useSearchParams } from 'react-router-dom';
 import { UserTokenContext, fetchProfile } from '../scripts/api';
 import { useQuery } from 'react-query';
 
+interface SpotifyUser {
+  country: string;
+  display_name: string;
+  email: string;
+  explicit_content: {
+    filter_enabled: boolean;
+    filter_locked: boolean;
+  };
+  external_urls: {
+    spotify: string;
+  };
+  followers: {
+    href: string;
+    total: number;
+  };
+  href: string;
+  id: string;
+  images: {
+    url: string;
+    height: number;
+    width: number;
+  }[];
+  product: string;
+  type: string;
+  uri: string;
+}
+
 function Callback() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const { loginWithSpotify, accessToken } = useContext(UserTokenContext);
   const code = searchParams.get('code');
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState<SpotifyUser[]>();
   const { isLoading } = useQuery(
     ['taskLists'],
     async () => await fetchProfile(accessToken),
@@ -30,9 +57,11 @@ function Callback() {
     );
   }
 
+  console.log(user);
+
   return (
     <div className="flex flex-col h-screen justify-center items-center">
-      Hello there {user.display_name}
+      Hello there {user ? user.display_name : 'Guest'}
     </div>
   );
 }
