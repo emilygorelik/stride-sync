5ddc861a016b4a98004afe96056519067a9c92db
Emily Gorelik <egorelik@jahnelgroup.com>
2023-10-03 10:41:32 -0400

    created playlist component

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,15 +1,33 @@
 import { Card } from '../components/Card';
+import { Playlist } from '../components/Playlist';
 import { RunDetailsBlock } from '../components/RunDetailsBlock';
 import { StrideDetailsBlock } from '../components/StrideDetailsBlock';
 import { SubmitButton } from '../components/SubmitButton';
 
 function Home() {
   return (
-    <div className="flex w-3/4 m-auto justify-center flex-col items-center">
-      <h1 className="text-3xl font-bold my-8">Spotify StrideSync</h1>
-      <div className="flex w-full gap-8 px-4">
-        <Card addClass="w-1/2">
+    <div className="flex w-3/4 h-screen m-auto p-8 flex-col items-center">
+      <h1 className="text-3xl font-bold">Spotify StrideSync</h1>
+      <div className="flex w-full overflow-hidden gap-8 px-4">
+        <Card addClass="flex w-1/2">
           <h2>Select Playlist</h2>
+          <div className="flex flex-col w-full gap-4 overflow-y-auto scrollbar-w-2 scrollbar-track-gray-100 scrollbar-thumb-indigo-600">
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            bottom
+          </div>
           {/* TODO: create playlist display component */}
           {/* map all playlists to this component */}
         </Card>
