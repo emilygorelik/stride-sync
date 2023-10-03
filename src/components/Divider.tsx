fe3238773fa419cafe4d41c0248c98e1e1d89cd8
Emily Gorelik <egorelik@jahnelgroup.com>
2023-10-03 10:40:40 -0400

    replaced divider with daisy one

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,9 +1,3 @@
 export function Divider() {
-  return (
-    <div className="flex flex-col items-center">
-      <div className="h-full w-px bg-white" />
-      <div className="mx-4 font-bold text-white">or</div>
-      <div className="h-full w-px bg-white" />
-    </div>
-  );
+  return <div className="divider divider-horizontal">or</div>;
 }
