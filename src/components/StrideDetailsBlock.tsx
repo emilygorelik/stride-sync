f85adca8f99b88f7dbaa663ccae69fa56929ef2e
Emily Gorelik <egorelik@jahnelgroup.com>
2023-10-04 16:29:22 -0400

    track stride and height and log to console on change

--- src/components/StrideDetailsBlock.tsx
+++ src/components/StrideDetailsBlock.tsx
@@ -4,7 +4,8 @@ import { NumberInput } from './NumberInput';
 import { RadioGroup } from './RadioGroup';
 
 export function StrideDetailsBlock() {
-  const [stride, setStride] = useState<string>('test');
+  const [stride, setStride] = useState<string>('');
+  const [height, setHeight] = useState<string>('');
 
   const handleStrideChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
@@ -12,22 +13,25 @@ export function StrideDetailsBlock() {
   };
   console.log('the stride is: ', stride);
 
+  const handleHeightChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setHeight(value);
+  };
+  console.log('the height is: ', height);
+
   return (
     <div className="flex w-full flex-col ">
       <h3>Stride Details</h3>
       <div className="flex">
         <div className="form-control w-fit">
           <span className="label-text">Stride Length</span>
-          <NumberInput
-            dummyText="00.00"
-            onChange={handleStrideChange}
-          ></NumberInput>
+          <NumberInput dummyText="00.00" onChange={handleStrideChange} />
           <RadioGroup options={['inches', 'centimeters']} groupName="stride" />
         </div>
         <Divider />
         <div className="form-control w-fit">
           <span className="label-text">Height</span>
-          <NumberInput dummyText="00.00" />
+          <NumberInput dummyText="00.00" onChange={handleHeightChange} />
           <RadioGroup options={['inches', 'centimeters']} groupName="height" />
         </div>
       </div>
