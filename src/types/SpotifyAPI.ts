5ef64eaff93bae538a3f1eb6a382ca6304130fbc
Emily Gorelik <egorelik@jahnelgroup.com>
2023-10-12 10:22:35 -0400

    types for audio features

--- src/types/SpotifyAPI.ts
+++ src/types/SpotifyAPI.ts
@@ -151,3 +151,28 @@ interface SpotifyTrack {
   uri: string;
   is_local: boolean;
 }
+
+interface SpotifyAudioFeature {
+  acousticness: number;
+  analysis_url: string;
+  danceability: number;
+  duration_ms: number;
+  energy: number;
+  id: string;
+  instrumentalness: number;
+  key: number;
+  liveness: number;
+  loudness: number;
+  mode: number;
+  speechiness: number;
+  tempo: number;
+  time_signature: number;
+  track_href: string;
+  type: string;
+  uri: string;
+  valence: number;
+}
+
+interface SpotifyAudioFeatures {
+  audio_features: SpotifyAudioFeature[];
+}
