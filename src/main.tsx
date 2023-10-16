f6852e728f8caccd78aa630094a38bd99011eb7a
Emily Gorelik <egorelik@jahnelgroup.com>
2023-10-16 13:46:08 -0400

    can publish login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { UserTokenProvider } from './api';
 import './index.css';
+// import Callback from './screens/callback';
+// import Home from './screens/home';
 import Login from './screens/login';
 
 const queryClient = new QueryClient();
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <Login />,
-//   },
-//   {
-//     path: '/callback',
-//     element: <Callback />,
-//   },
-//   {
-//     path: '/home',
-//     element: <Home />,
-//   },
-// ]);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <UserTokenProvider>
-        <Login />
-        {/* <RouterProvider router={router} /> */}
+      <UserTokenProvider>  
+        <Login/>
+        {/* <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/callback" element={<Callback />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </Router> */}
       </UserTokenProvider>
     </QueryClientProvider>
   </React.StrictMode>,
+  document.getElementById('root')
 );
