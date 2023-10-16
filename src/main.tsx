fdd1c085592c69e2adf8d00d5ffda46f0e3325aa
Emily Gorelik <egorelik@jahnelgroup.com>
2023-10-16 13:27:12 -0400

    Revert "Merge pull request #23 from emgore-jg/SSS-deploy-phase2"
    This reverts commit 2bb3bada494be395dfe8e5687657ac2aa198f757, reversing
    changes made to 0a0f2bc69e15cb5e9eca353c8258f0547105ea24.

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { UserTokenProvider } from './api';
-import './index.css';
-import Callback from './screens/callback';
-import Home from './screens/home';
-import Login from './screens/login';
-
-const queryClient = new QueryClient();
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login />,
-  },
-  {
-    path: '/callback',
-    element: <Callback />,
-  },
-  {
-    path: '/home',
-    element: <Home />,
-  },
-]);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <UserTokenProvider>
-        <RouterProvider router={router} />
-      </UserTokenProvider>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
-);
+)
