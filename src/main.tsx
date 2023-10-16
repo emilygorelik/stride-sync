d3eafb67763f153cf03a8c01dcd564b7f309b2a9
Emily Gorelik <egorelik@jahnelgroup.com>
2023-10-16 12:52:15 -0400

    can publish login page but redirects not working

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { UserTokenProvider } from './api';
 import './index.css';
-import Callback from './screens/callback';
-import Home from './screens/home';
 import Login from './screens/login';
 
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Login />,
-  },
-  {
-    path: '/callback',
-    element: <Callback />,
-  },
-  {
-    path: '/home',
-    element: <Home />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Login />,
+//   },
+//   {
+//     path: '/callback',
+//     element: <Callback />,
+//   },
+//   {
+//     path: '/home',
+//     element: <Home />,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <UserTokenProvider>
-        <RouterProvider router={router} />
+        <Login />
+        {/* <RouterProvider router={router} /> */}
       </UserTokenProvider>
     </QueryClientProvider>
   </React.StrictMode>,
