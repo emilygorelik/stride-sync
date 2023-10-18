1709124e715a97b607902a7f685f628fb95f5ce9
Emily Gorelik <egorelik@jahnelgroup.com>
2023-10-18 11:10:11 -0400

    don't need router at all

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import { UserTokenProvider } from './api';
 import './index.css';
-import Callback from './screens/callback';
 import Login from './screens/login';
 
 const queryClient = new QueryClient();
@@ -13,13 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <UserTokenProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/callback" element={<Callback />} />
-            {/* <Route path="/home" element={<Home />} /> */}
-          </Routes>
-        </Router>
+        <Login />
       </UserTokenProvider>
     </QueryClientProvider>
   </React.StrictMode>,
